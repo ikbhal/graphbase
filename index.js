64a33a050befa311eaebe1e52f95dfce7f5bbdc0
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2023-08-12 11:30:40 +0530

    remove loadGraphFromJson from index.js

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ app.post('/addEdge', (req, res) => {
   // Respond with success message
   res.json({
     success: true,
-    message: 'Edge added successfully',
+    message: 'Edge added successfully', 
   });
 });
 
@@ -229,46 +229,6 @@ app.delete('/deleteProperty/:nodeKey/:propertyKey', (req, res) => {
     });
   }
 });
-
-function loadGraphFromJson(filePath) {
-  try {
-
-    // Check if the file path exists
-    if (!fs.existsSync(filePath)) {
-      console.log('JSON file does not exist. Returning empty graph.');
-      return new graphology.Graph();
-    }
-
-    const rawData = fs.readFileSync(filePath, 'utf8');
-    const graphData = JSON.parse(rawData);
-
-    // Create a new graph instance
-    const graph = new graphology.Graph();
-
-    // Add nodes and their attributes
-    graphData.nodes.forEach(nodeData => {
-      graph.addNode(nodeData.nodeKey, nodeData.properties);
-    });
-
-    // Add edges and their attributes
-    graphData.edges.forEach(edgeData => {
-      graph.addEdgeWithKey(
-        edgeData.edgeKey,
-        edgeData.sourceNodeKey,
-        edgeData.targetNodeKey,
-        edgeData.properties
-      );
-    });
-
-    console.log('Graph loaded successfully from JSON file.');
-
-    return graph;
-  } catch (error) {
-    console.error('Error loading graph from JSON file:', error);
-    return null;
-  }
-}
-
 
 //v 5
 
